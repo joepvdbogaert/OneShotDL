3c633da66a17301ffb1604dcdaeebffa70b11847
joepvdbogaert <joepvdbogaert@gmail.com>
2018-05-02 10:18:09 +0200

    Fixed small mistake in run file

--- run_autoencoder.py
+++ run_autoencoder.py
@@ -8,9 +8,9 @@ Created on Mon Mar 12 17:17:35 2018
 import resource
 resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
 
-from models import OneShotTransferCNN
+from models import OneShotAutoencoder
 
-model = OneShotTransferCNN(log=True, verbose=0)
+model = OneShotAutoencoder(log=True, verbose=0)
 max_evals = 200
 
 model.tune_with_HORD(max_evals)
