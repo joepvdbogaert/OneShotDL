5c022a5626794c5acd92e666a65e7750991d32ca
joepvdbogaert <joepvdbogaert@gmail.com>
2018-07-19 09:09:04 +0200

    Changed run files

--- run_oneshotcnn.py
+++ run_oneshotcnn.py
@@ -5,12 +5,6 @@ Created on Mon Mar 12 17:17:35 2018
 
 @author: Joep van den Bogaert
 """
-import resource
-resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
-
 from models import OneShotCNN
-
-model = OneShotCNN(log=True, verbose=1)
-max_evals = 200
-
-model.tune_with_HORD(max_evals)
+model = OneShotCNN(log=True, verbose=0)
+model.tune_with_HORD(200)
--- run_transfercnn.py
+++ run_transfercnn.py
@@ -5,12 +5,9 @@ Created on Mon Mar 12 17:17:35 2018
 
 @author: Joep van den Bogaert
 """
-import resource
-resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
-
 from models import OneShotTransferCNN
 
 model = OneShotTransferCNN(log=True, verbose=0)
-max_evals = 200
+max_evals = 300
 
 model.tune_with_HORD(max_evals)
